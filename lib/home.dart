2838d153d6aaae04d3042511be6376ea7594f223
Narassin <37112149+Narassin@users.noreply.github.com>
2022-06-05 20:19:05 +0800

    Home update

---
+++ lib/home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class TabBox extends StatelessWidget {
+  TabBox();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: const [
+          Text('All',
+              style: TextStyle(
+                fontSize: 21,
+                color: Colors.white,
+              )),
+          IconButton(
+              onPressed: null,
+              icon: Icon(Icons.more_horiz),
+              tooltip: 'More Option'),
+        ],
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+      margin: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 20),
+      color: Colors.white30,
+    );
+  }
+}
