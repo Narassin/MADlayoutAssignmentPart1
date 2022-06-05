88d2f38c4daf57c24cf487e005143c722b1d2a13
Narassin <37112149+Narassin@users.noreply.github.com>
2022-06-05 20:18:34 +0800

    Built homepage and base for grid

---
+++ lib/griddb.dart
@@ -0,0 +1,8 @@
+var iotDB = [
+  //item 1
+  {'deviceName': 'Master Bedroom', 'icon': ''},
+  //item 1
+  {'deviceName': 'Living Room', 'icon': ''},
+  //item 1
+  {'deviceName': 'Studio Computer', 'icon': 'computer'},
+];
